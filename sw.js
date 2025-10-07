b3093ea8b99da459872e287d47d17720ed399926
shivam15112003 <147326148+shivam15112003@users.noreply.github.com>
2025-10-07 15:25:19 -0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+// Tiny offline cache
+const CACHE = 'shivam-portfolio-v1';
+const ASSETS = [
+  './', './index.html', './profile.jpeg', './ASU_Resume_Template_Shivam.pdf'
+];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))) );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e=>{
+  const req = e.request;
+  if(req.method!=='GET') return;
+  e.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(res=>{
+      const copy = res.clone();
+      caches.open(CACHE).then(c=>c.put(req, copy));
+      return res;
+    }).catch(()=> caches.match('./')))
+  );
+});
